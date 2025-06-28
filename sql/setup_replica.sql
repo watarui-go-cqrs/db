bdcf07f75d5127febaf29b5af524e98f1f7645bb
Wataru Ishikawa <wataruby@gmail.com>
2025-06-28 15:31:10 +0900

    init

---
+++ sql/setup_replica.sql
@@ -0,0 +1,21 @@
+-- Replica server setup (run on query_db)
+-- Stop any existing replication
+STOP REPLICA;
+
+-- Reset replica configuration completely
+RESET REPLICA ALL;
+
+-- Configure the replica to connect to master
+CHANGE MASTER TO
+  MASTER_HOST='command_db',
+  MASTER_PORT=3306,
+  MASTER_USER='repl',
+  MASTER_PASSWORD='password',
+  MASTER_LOG_FILE='mysql-bin.000003',
+  MASTER_LOG_POS=157;
+
+-- Start replication
+START REPLICA;
+
+-- Check replication status
+SHOW REPLICA STATUS\G
